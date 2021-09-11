fdf259298e540d620f925fbfcda3611407c3487d
Dmitresso <dmitresso@gmail.com>
2021-09-11 20:40:14 +0700

    Introduction to the Rigidbody in Unity/Using the Force

--- Assets/Intro_Rigidbody/Code/Scripts/Forces.cs
+++ Assets/Intro_Rigidbody/Code/Scripts/Forces.cs
@@ -1,20 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 using IndiePixel;
 
-public class Forces : IP_BaseRB_Controller
-{
+
+public class Forces : MonoBehaviour {
     #region Variables
     public float maxSpeed = 1f;
     public Vector3 movementDirection = new Vector3(0f, 0f, 1f);
+
+    private Rigidbody rb;
     #endregion
 
 
+    
+    #region Builtin Methods
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    private void FixedUpdate() {
+        if (!rb) return;
+        HandlePhysics(movementDirection, maxSpeed);
+    }
+    #endregion
+    
+    
+    
     #region Custom Methods
-    protected override void HandlePhysics()
-    {
-        rb.AddForce(movementDirection * maxSpeed);
+    protected void HandlePhysics(Vector3 direction, float speed) {
+        rb.AddForce(direction * speed);
     }
     #endregion
 }
