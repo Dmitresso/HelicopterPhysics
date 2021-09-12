ac8240a136ccf11fcf2d29d41786f979ec813061
Dmitresso <dmitresso@gmail.com>
2021-09-12 17:28:20 +0700

    Introduction to the Rigidbody in Unity/Understanding Weight

--- Assets/Intro_Rigidbody/Code/Scripts/Weight.cs
+++ Assets/Intro_Rigidbody/Code/Scripts/Weight.cs
@@ -1,39 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Weight : MonoBehaviour
-{
+
+public class Weight : MonoBehaviour {
     #region Variables
     [Header("Weight Properties")]
     public float weightInLbs = 10f;
-
     public float weight;
-
-
-    const float lbsToKg = 0.454f;
-    const float kgToLbs = 2.205f;
 
     private Rigidbody rb;
     #endregion
 
+
+    
+    #region Constants
+    const float lbsToKg = 0.454f;
+    const float kgToLbs = 2.205f;
+    #endregion
+    
+    
+    
     #region Builtin Methods
-    // Use this for initialization
-    void Start ()
-    {
-        float finalKG = weightInLbs * lbsToKg;
+    private void Start () {
+        var finalKG = weightInLbs * lbsToKg;
         weight = finalKG;
         rb = GetComponent<Rigidbody>();
-        if (rb)
-        {
-            rb.mass = finalKG;
-        }
+        if (rb) rb.mass = finalKG;
     }
-	
-	// Update is called once per frame
-	void FixedUpdate ()
-    {
-		
-	}
     #endregion
 }
