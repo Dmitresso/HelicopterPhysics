4d5ede94a146147eb7f1298c43849cb1c0055aa5
Dmitresso <dmitresso@gmail.com>
2021-09-15 15:26:21 +0700

    Animating the Propeller/Blurring the Rotors/Part 2

--- Assets/HelicopterPhysics/Code/Scripts/Rotors/RotorBlur.cs
+++ Assets/HelicopterPhysics/Code/Scripts/Rotors/RotorBlur.cs
@@ -1,18 +1,24 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace WheelApps {
     public class RotorBlur : MonoBehaviour, IHelicopterRotor {
         #region Variables
         [Header("Rotor Blur Properties")]
-        public GameObject lRotor;
-        public GameObject rRotor;
+        public float maxDps = 1000f;
+        public List<GameObject> blades = new List<GameObject>();
         public GameObject blurGeo;
+        public List<Texture2D> blurTextures = new List<Texture2D>(); 
         #endregion
         
         
         
         #region Interface Methods
         public void UpdateRotor(float dps, InputController input) {
+            var normalizedDPS = Mathf.InverseLerp(0f, maxDps, dps);
+            var blurTexId = Mathf.FloorToInt(normalizedDPS * blurTextures.Count - 1);
+            blurTexId = Mathf.Clamp(blurTexId, 0, blurTextures.Count - 1);
+            Debug.Log(blurTexId);
         }
         #endregion
 
