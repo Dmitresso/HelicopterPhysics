58a963bb9c4f92b736bb2e6beb8527c03e95c846
Dmitresso <dmitresso@gmail.com>
2021-09-17 19:38:35 +0700

    Building the Helicopter Camera/Basic Follow Camera

--- Assets/HelicopterPhysics/Code/Scripts/Camera/HelicopterCamera.cs
+++ Assets/HelicopterPhysics/Code/Scripts/Camera/HelicopterCamera.cs
@@ -6,8 +6,13 @@ namespace WheelApps {
         #region Variables
         [Header("Camera Properties")]
         public Rigidbody rb;
+        public Transform lookAtTarget;
         public float height = 2f;
         public float distance = 2f;
+        public float smoothSpeed = 0.35f;
+
+        private Vector3 targetPosition;
+        private Vector3 refVelocity;
         #endregion
 
 
@@ -22,7 +27,17 @@ namespace WheelApps {
         
         #region Interface Methods
         public void UpdateCamera() {
+            var transform = this.transform;
+            var rbPosition = rb.position;
             
+            var flatForward = rb.transform.forward;
+            flatForward.y = 0f;
+            flatForward = flatForward.normalized;
+
+            targetPosition = rbPosition + flatForward * distance + Vector3.up * height;
+
+            transform.position = Vector3.SmoothDamp(rbPosition, targetPosition, ref refVelocity, smoothSpeed);
+            transform.LookAt(lookAtTarget);
         }
         #endregion
     }
