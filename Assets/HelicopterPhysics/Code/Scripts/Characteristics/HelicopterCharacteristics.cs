79e632f65b79df3227b11dc8b8370e97e571ff46
Dmitresso <dmitresso@gmail.com>
2021-09-17 11:49:30 +0700

    Flight Characteristics/Creating Lift/Part 2

--- Assets/HelicopterPhysics/Code/Scripts/Characteristics/HelicopterCharacteristics.cs
+++ Assets/HelicopterPhysics/Code/Scripts/Characteristics/HelicopterCharacteristics.cs
@@ -3,9 +3,21 @@ using UnityEngine;
 namespace WheelApps {
     public class HelicopterCharacteristics : MonoBehaviour {
         #region Variables
+        [Header("Lift Properties")]
+        public float maxLiftForce = 10f;
+        public MainHelicopterRotor mainRotor;
         #endregion
 
 
+
+        #region Builtin Methods
+        private void Start() {
+            
+        }
+        #endregion
+        
+        
+        
         #region Custom Methods
         public void UpdateCharacteristics(Rigidbody rb, InputController input) {
             HandleLift(rb, input);
@@ -15,8 +27,11 @@ namespace WheelApps {
 
 
         protected virtual void HandleLift(Rigidbody rb, InputController input) {
-            var liftForce = transform.up * (Physics.gravity.magnitude * rb.mass);
-            rb.AddForce(liftForce, ForceMode.Force);
+            if (!mainRotor) return;
+            var liftForce = (Physics.gravity.magnitude + maxLiftForce) * rb.mass * transform.up;
+            var normalizedRPM = mainRotor.CurrentRPM * 0.05f;
+            var finalForce = Mathf.Pow(normalizedRPM, 2f) * Mathf.Pow(input.StickyCollective, 2f) * liftForce;
+            rb.AddForce(finalForce, ForceMode.Force);
         }
         
         
