0f440531893bcccf10d8c0be86e6a69b9e8f431e
Dmitresso <dmitresso@gmail.com>
2021-09-18 15:58:44 +0700

    Building a Better Heli Camera/Stubbing in the Script

---
+++ Assets/HelicopterPhysics/Code/Scripts/Camera/AdvancedHelicopterCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+namespace WheelApps {
+    public class AdvancedHelicopterCamera : MonoBehaviour, IHelicopterCamera {
+        #region Variables
+        
+        #endregion
+
+
+
+        #region Interface Methods
+        public void UpdateCamera() {
+            
+        }
+        #endregion
+    }
+}
