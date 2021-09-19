5c1a1924fdea6c79daf169d788399643cb95f678
Dmitresso <dmitresso@gmail.com>
2021-09-19 17:02:32 +0700

    Arcade Heli Physics - Updating the Setup/Updating the Helicopter Menu Setup

--- Assets/HelicopterPhysics/Code/Editor/Menus/HelicopterMenus.cs
+++ Assets/HelicopterPhysics/Code/Editor/Menus/HelicopterMenus.cs
@@ -9,18 +9,44 @@ namespace WheelApps {
             var currentHelicopter = new GameObject("Helicopter", typeof(HelicopterController));
             var currentCOM = new GameObject("COM");
             var audioGRP = new GameObject("Audio_GRP");
-            var graphicsGRP = new GameObject("Graphics_GRP");
+            var graphicsGRP = new GameObject("Graphic_GRP");
             var collisionGRP = new GameObject("Collision_GRP");
+            var engineGRP = new GameObject("Engine_GRP");
+            var rotorGRP = new GameObject("Rotor_GRP");
+            
+            SetupRotorGRP(rotorGRP);
+            SetupEngineGRP(engineGRP);
+            
             var currentController = currentHelicopter.GetComponent<HelicopterController>();
 
             currentCOM.transform.SetParent(currentHelicopter.transform);
             audioGRP.transform.SetParent(currentHelicopter.transform);
             graphicsGRP.transform.SetParent(currentHelicopter.transform);
             collisionGRP.transform.SetParent(currentHelicopter.transform);
-            
+            engineGRP.transform.SetParent(currentHelicopter.transform);
+            rotorGRP.transform.SetParent(currentHelicopter.transform);
+
             currentController.com = currentCOM.transform;
-            
+
             Selection.activeGameObject = currentHelicopter;
+        }
+
+
+        public static void SetupRotorGRP(GameObject rotorGO) {
+            rotorGO.AddComponent<RotorController>();
+            var mainGRP = new GameObject("MainRotor");
+            var tailGRP = new GameObject("TailRotor");
+            mainGRP.AddComponent<MainHelicopterRotor>();
+            tailGRP.AddComponent<TailHelicopterRotor>();
+            mainGRP.transform.SetParent(rotorGO.transform);
+            tailGRP.transform.SetParent(rotorGO.transform);
+        }
+
+
+        public static void SetupEngineGRP(GameObject engineGO) {
+            var engineGRP = new GameObject("MainEngine");
+            engineGRP.AddComponent<HelicopterEngine>();
+            engineGRP.transform.SetParent(engineGO.transform);
         }
     }
 }
