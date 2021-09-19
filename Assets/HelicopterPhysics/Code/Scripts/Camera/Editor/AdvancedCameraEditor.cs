f57ea4cf0e1d1eef108b34cffd9b1224d7c1758b
Dmitresso <dmitresso@gmail.com>
2021-09-19 10:21:23 +0700

    Building the Helicopter Camera/Visualizing the Min/Max Range

---
+++ Assets/HelicopterPhysics/Code/Scripts/Camera/Editor/AdvancedCameraEditor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+
+namespace WheelApps {
+    [CustomEditor(typeof(AdvancedCamera))]
+    public class AdvancedCameraEditor : Editor {
+        #region Variables
+        private AdvancedCamera targetCamera;
+        #endregion
+
+
+
+        #region Methods
+        private void OnEnable() {
+            targetCamera = (AdvancedCamera) target;
+        }
+
+
+        private void OnSceneGUI() {
+            var minDistance = targetCamera.minDistance;
+            var maxDistance = targetCamera.maxDistance;
+            var targetForward = targetCamera.rb.transform.forward;
+            
+            
+            Handles.color = Color.blue;
+            Handles.DrawWireDisc(targetCamera.rb.position, Vector3.up, minDistance);
+            Handles.DrawWireDisc(targetCamera.rb.position, Vector3.up, maxDistance);
+
+            targetCamera.minDistance = Handles.ScaleSlider(targetCamera.minDistance, targetCamera.rb.position, Vector3.forward + targetForward * minDistance, Quaternion.identity, 1f, 0f);
+            targetCamera.maxDistance = Handles.ScaleSlider(targetCamera.maxDistance, targetCamera.rb.position, Vector3.back - targetForward * maxDistance, Quaternion.identity, 1f, 0f);
+        }
+        #endregion
+    }
+}
