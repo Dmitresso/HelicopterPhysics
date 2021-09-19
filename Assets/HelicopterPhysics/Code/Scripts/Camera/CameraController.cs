13f7a1f387a78c0b9c8ac99c18e8859ea1df5154
Dmitresso <dmitresso@gmail.com>
2021-09-19 10:51:41 +0700

    Creating a Camera Controller/Stubbing in the Script

---
+++ Assets/HelicopterPhysics/Code/Scripts/Camera/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+namespace WheelApps {
+    public class CameraController : MonoBehaviour {
+        #region Variables
+        public List<BaseCamera> cameras = new List<BaseCamera>();
+        #endregion
+
+
+
+        #region Builtin Methods
+
+        private void Start() {
+            cameras = GetComponentsInChildren<BaseCamera>().ToList();
+            foreach (var camera in cameras) {
+                Debug.Log(camera.gameObject.name);
+            }
+        }
+
+        #endregion
+
+
+
+        #region Custom Methods
+
+        #endregion
+    }
+}
