1ad10ca26311474e4587cc5913af50220859c6dc
Dmitresso <dmitresso@gmail.com>
2021-09-21 19:23:36 +0700

    Arcade Heli Physics - Creating a Top Down Camera/Stub in the Scripts

--- Assets/HelicopterPhysics/Code/Scripts/Camera/TopDownCamera.cs
+++ Assets/HelicopterPhysics/Code/Scripts/Camera/TopDownCamera.cs
@@ -1,6 +1,33 @@
-﻿namespace WheelApps {
-    public class TopDownCamera : BaseCamera {
+﻿using UnityEngine;
+
+
+namespace WheelApps {
+    public class TopDownCamera : BaseCamera, IHelicopterCamera {
         #region Variables
+        [Header("Top Down Camera Properties")]
+        public float height = 4f;
+        public float distance = 4f;
+        #endregion
+
+
+
+        #region Builtin Methods
+        private void OnEnable() {
+            updateEvent.AddListener(UpdateCamera);
+        }
+
+
+        private void OnDisable() {
+            updateEvent.RemoveListener(UpdateCamera);
+        }
+        #endregion
+        
+        
+
+        #region Interface Methods
+        public void UpdateCamera() {
+            
+        }
         #endregion
     }
 }
