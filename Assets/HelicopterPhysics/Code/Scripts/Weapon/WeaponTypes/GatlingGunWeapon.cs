a06449d046a534c91177be49a936f76778f8c45f
Dmitresso <dmitresso@gmail.com>
2021-09-26 13:01:12 +0700

    Arcade Heli Weapons - Creating the Gatling Gun Functionality/Animating the Gatling Gun

--- Assets/HelicopterPhysics/Code/Scripts/Weapon/WeaponTypes/GatlingGunWeapon.cs
+++ Assets/HelicopterPhysics/Code/Scripts/Weapon/WeaponTypes/GatlingGunWeapon.cs
@@ -1,21 +1,41 @@
 using UnityEngine;
 
+
 namespace WheelApps {
     public class GatlingGunWeapon : RapidFireWeapon {
         #region Variables
         [Header("Gatling Gun Properties")]
         public Transform barrelGO;
+        public float rotationSpeed = 15f;
+        public float slowDownSpeed = 5f;
 
-        public float rotationSpeed = 10f;
-
+        private float lastRotationSpeed;
         #endregion
 
 
 
+        #region Builtin Methods
+        private void Update() {
+            if (barrelGO) {
+                lastRotationSpeed -= slowDownSpeed * Time.deltaTime;
+                lastRotationSpeed = Mathf.Clamp(lastRotationSpeed, 0f, rotationSpeed);
+                barrelGO.Rotate(Vector3.up, lastRotationSpeed);
+            }
+        }
+        #endregion
+        
+        
+        
+
         #region Override Methods
         public override void FireWeapon() {
             base.FireWeapon();
-            
+
+            if (barrelGO) {
+                barrelGO.Rotate(Vector3.up, rotationSpeed);
+                lastRotationSpeed = rotationSpeed;
+            }
+
         }
         #endregion
     }
